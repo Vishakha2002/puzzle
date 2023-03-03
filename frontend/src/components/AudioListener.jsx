277e7b6df1952df84b1bc4c7f64cc372d116dc23
Vishakha2002 <vishakhatyagi109@gmail.com>
2023-03-03 22:00:29 +0530

    Added Keyboard Controls and API call

--- frontend/src/components/AudioListener.jsx
+++ frontend/src/components/AudioListener.jsx
@@ -4,20 +4,7 @@ import "./Audio.css"
 
 const AudioListener = () => {
  const [message, setMessage] = useState('');
- const commands = [
-   {
-     command: 'reset',
-     callback: () => resetTranscript()
-   },
-   {
-     command: 'shut up',
-     callback: () => setMessage('I wasn\'t talking.')
-   },
-   {
-     command: 'Hello',
-     callback: () => setMessage('Hi there!')
-   },
- ]
+ const commands = [] // can be used to return trained responses if we want, otherwise kept blank
  const {
    transcript,
    interimTranscript,
@@ -26,13 +13,50 @@ const AudioListener = () => {
    listening,
  } = useSpeechRecognition({ commands });
 
+ const listenContinuously = () => {
+  SpeechRecognition.startListening({
+    continuous: true,
+    language: 'en-GB',
+  });
+};
+
+ const handleKeypress = event => {
+    if (event.key === "a") {
+      // start audio recording
+      resetTranscript()
+      listenContinuously()
+    }
+    if (event.key === "s") {
+      // stop audio recording
+      SpeechRecognition.stopListening()
+    }
+    if (event.key === "p") {
+      // stop audio recording and hide component as well
+      SpeechRecognition.stopListening()
+    }
+  }
+
+  window.removeEventListener("keypress", handleKeypress)
+  window.addEventListener("keypress", handleKeypress)
+
  useEffect(() => {
    if (finalTranscript !== '') {
         // Make API Call to backend here with user Query using finalTranscript
-        console.log('User Query', finalTranscript);
+        console.log('User Query->', finalTranscript);
 
+        // Uncomment this code for API call
+        // axios.post('/api/trigger_model', {
+        //   question: finalTranscript}, {
+        //   headers: "Content-Type: application/json"}
+        // ).then(response => {
+        //   updateModelLoadingInProgress()
+        //   console.log(response)
+        //   setModelResponse("Your answer is: " + response.data["answer"])
+        // }).catch(error => {
+        //   console.log(error)
+        // })
 
-        // Reset finalTranscript after API call
+        // Resetting User Query(finalTranscript) after API call
         resetTranscript()
    }
  }, [interimTranscript, finalTranscript]);
@@ -43,19 +67,13 @@ const AudioListener = () => {
  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
    console.log('Your browser does not support speech recognition software! Try Chrome desktop, maybe?');
  }
- const listenContinuously = () => {
-   SpeechRecognition.startListening({
-     continuous: true,
-     language: 'en-GB',
-   });
- };
+ 
  return (
     <div id="outer">
      <div className="inner">
        <div>
-         <button type="button" onClick={resetTranscript}>Reset</button>
-         <button type="button" onClick={listenContinuously}>Listen</button>
-         <button type="button" onClick={SpeechRecognition.stopListening}>Stop</button>
+          <button type="button" onClick={listenContinuously}>Ask</button>
+          <button type="button" onClick={resetTranscript}>Reset</button>
        </div>
      </div>
      <div className="inner">
